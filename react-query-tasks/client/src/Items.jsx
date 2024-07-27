41604d8709242aed1eb4edf2f92671c665c32118
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-27 08:00:25 -0700

    initial commit

---
+++ react-query-tasks/client/src/Items.jsx
@@ -0,0 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
+import SingleItem from './SingleItem';
+import customFetch from './utils';
+
+const Items = () => {
+  const { isLoading, data, error, isError } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const {data} = await customFetch.get('/');
+      return data;
+    },
+  });
+  
+  if (isLoading) {
+    return <p style={{ marginTop: '1rem ' }}>Loading...</p>;
+  }
+
+  if (error) {
+    return <p style={{ marginTop: '1rem ' }}>An error occurred: {error.message}</p>;
+  }
+  
+  if (!data?.taskList?.length) {
+    return <p style={{ marginTop: '1rem ' }}>No tasks found.</p>;
+  }
+
+  return (
+    <div className='items'>
+      {data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items;
---
+++ react-query-tasks/client/src/SingleItem.jsx
@@ -0,0 +1,33 @@
+import { useEditTask, useDeleteTask } from "./hooks/tasks";
+
+const SingleItem = ({ item }) => {
+  const { editTask } = useEditTask();
+  const { deleteTask, deleteTaskLoading } = useDeleteTask();
+
+  return (
+    <div className='single-item'>
+      <input
+        type='checkbox'
+        checked={item.isDone}
+        onChange={() => editTask({taskId: item.id, isDone: !item.isDone})}
+      />
+      <p
+        style={{
+          textTransform: 'capitalize',
+          textDecoration: item.isDone && 'line-through',
+        }}
+      >
+        {item.title}
+      </p>
+      <button
+        className='btn remove-btn'
+        type='button'
+        onClick={() => deleteTask({taskId: item.id})}
+        disabled={deleteTaskLoading}
+      >
+        delete
+      </button>
+    </div>
+  );
+};
+export default SingleItem;
