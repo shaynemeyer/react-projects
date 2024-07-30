13861e2c4037edd656e3b45befcb6b9614600904
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-29 18:21:02 -0700

    added navbar

---
+++ comfy-store/src/components/ToggleTheme.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
+
+function ToggleTheme() {
+  const [theme, setTheme] = useState(false);
+
+  const handleTheme = () => {
+    setTheme(!theme);
+  };
+
+  return (
+    <div className="navbar-end">
+      <label className="swap swap-rotate ">
+        {/* this hidden checkbox controls the state */}
+        <input type="checkbox" onChange={handleTheme} />
+
+        {/* sun icon */}
+        <BsSunFill className="swap-on h-4 w-4" />
+
+        {/* moon icon */}
+        <BsMoonFill className="swap-off h-4 w-4" />
+      </label>
+    </div>
+  );
+}
+
+export default ToggleTheme;
