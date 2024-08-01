8655b5426ce9fa4a8e053583d95413aab666af41
Shayne Meyer <shayne.meyer@gmail.com>
2024-08-01 12:50:09 -0700

    setup next-store

---
+++ next-store/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Container from "@/components/global/Container";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next Store",
+  description: "A nifty store built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        <Container className="py-20">{children}</Container>
+      </body>
+    </html>
+  );
+}
