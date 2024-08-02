6198b7d8e28f14f7932f0bafdaeebca892a29a21
Shayne Meyer <shayne.meyer@gmail.com>
2024-08-02 07:12:40 -0700

    add product components

--- next-store/app/page.tsx
+++ next-store/app/page.tsx
@@ -1,13 +1,16 @@
-import { Button } from "@/components/ui/button";
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import Hero from "@/components/home/Hero";
+import LoadingContainer from "@/components/global/LoadingContainer";
+import { Suspense } from "react";
 
 function HomePage() {
   return (
-    <div>
-      <h1 className="text-3xl">HomePage</h1>
-      <Button variant="outline" size="lg" className="capitalize m-8">
-        Click me
-      </Button>
-    </div>
+    <>
+      <Hero />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
+    </>
   );
 }
 export default HomePage;
--- next-store/next.config.mjs
+++ next-store/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lgxxmrmldkvzzzuntsic.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
