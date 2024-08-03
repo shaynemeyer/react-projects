f429400f1668065d4a02cb0b58f15fb2e3891f10
Shayne Meyer <shayne.meyer@gmail.com>
2024-08-03 09:27:37 -0700

    added loading table for product list in the admin

--- next-store/app/products/loading.tsx
+++ next-store/app/products/loading.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import LoadingContainer from "@/components/global/LoadingContainer";
+import LoadingTable from "@/components/global/LoadingTable";
 
 function loading() {
-  return <LoadingContainer />;
+  return <LoadingTable />;
 }
 export default loading;
