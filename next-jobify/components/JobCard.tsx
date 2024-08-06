fadc642f0e8f2ab9c4af3cd916125f928d972927
Shayne Meyer <shayne.meyer@gmail.com>
2024-08-06 10:24:59 -0700

    added database seed data and script

--- next-jobify/components/JobCard.tsx
+++ next-jobify/components/JobCard.tsx
@@ -41,7 +41,7 @@ function JobCard({ job }: { job: JobType }) {
         <Button asChild size="sm">
           <Link href={`/jobs/${job.id}`}>edit</Link>
         </Button>
-        <DeleteJobButton />
+        <DeleteJobButton id={job.id} />
       </CardFooter>
     </Card>
   );
---
+++ next-jobify/prisma/seed.js
@@ -0,0 +1,29 @@
+const { PrismaClient } = require("@prisma/client");
+const data = require("./mock-data.json");
+const prisma = new PrismaClient();
+
+async function main() {
+  const clerkId = "user_2kF3sh00UtmJWj2ueLFbX3kFCRD";
+  const jobs = data.map((job) => {
+    return {
+      ...job,
+      clerkId,
+    };
+  });
+
+  for (const job of jobs) {
+    await prisma.job.create({
+      data: job,
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
