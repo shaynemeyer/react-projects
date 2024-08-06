e5c30e96ee2cd5f72427240a6d1c98c451e16a10
Shayne Meyer <shayne.meyer@gmail.com>
2024-08-06 12:38:49 -0700

    added simple button container

--- next-jobify/components/JobsList.tsx
+++ next-jobify/components/JobsList.tsx
@@ -3,6 +3,7 @@ import JobCard from "./JobCard";
 import { useSearchParams } from "next/navigation";
 import { getAllJobsAction } from "@/utils/actions";
 import { useQuery } from "@tanstack/react-query";
+import ButtonContainer from "./ButtonContainer";
 
 function JobsList() {
   const searchParams = useSearchParams();
@@ -18,13 +19,24 @@ function JobsList() {
   });
   const jobs = data?.jobs || [];
 
+  const count = data?.count || 0;
+  const page = data?.page || 0;
+  const totalPages = data?.totalPages || 0;
+
   if (isPending) return <h2 className="text-xl">Please Wait...</h2>;
 
   if (jobs.length < 1) return <h2 className="text-xl">No Jobs Found...</h2>;
 
   return (
     <>
-      {/*button container  */}
+      <div className="flex items-center justify-between mb-8">
+        <h2 className="text-xl font-semibold capitalize ">
+          {count} jobs found
+        </h2>
+        {totalPages < 2 ? null : (
+          <ButtonContainer currentPage={page} totalPages={totalPages} />
+        )}
+      </div>
       <div className="grid md:grid-cols-2  gap-8">
         {jobs.map((job) => {
           return <JobCard key={job.id} job={job} />;
