d08223df61b7bb5c61f161031801a9363e2651c7
Shayne Meyer <shayne.meyer@gmail.com>
2024-08-06 10:43:13 -0700

    added getStats action

--- next-jobify/utils/actions.ts
+++ next-jobify/utils/actions.ts
@@ -163,3 +163,40 @@ export async function updateJobAction(
     return null;
   }
 }
+
+export async function getStatsAction(): Promise<{
+  pending: number;
+  interview: number;
+  declined: number;
+}> {
+  const userId = authenticateAndRedirect();
+  // just to show Skeleton
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  try {
+    const stats = await prisma.job.groupBy({
+      by: ["status"],
+      _count: {
+        status: true,
+      },
+      where: {
+        clerkId: userId, // replace userId with the actual clerkId
+      },
+    });
+
+    const statsObject = stats.reduce((acc, curr) => {
+      acc[curr.status] = curr._count.status;
+      return acc;
+    }, {} as Record<string, number>);
+
+    const defaultStats = {
+      pending: 0,
+      declined: 0,
+      interview: 0,
+      ...statsObject,
+    };
+
+    return defaultStats;
+  } catch (error) {
+    redirect("/jobs");
+  }
+}
